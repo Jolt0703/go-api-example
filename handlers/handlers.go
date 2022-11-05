739eb0e4abc89af468f486cedfb828743a69547c
Matsuzaki Jotaro <midori1136@gmail.com>
2022-11-06 02:37:16 +0900

    記事ハンドラにパスパラメターとクエリパラメータを追加

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 // /hello
@@ -16,14 +19,31 @@ func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
 	io.WriteString(w, "Posting Article...\n")
 }
 
-// /article/list
+// /article/list?page=[x]
 func ArticleListHandler(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Article List\n")
+	queryMap := req.URL.Query()
+	page := 1
+
+	if pageList, ok := queryMap["page"]; ok && len(pageList) > 0 {
+		var err error
+		page, err = strconv.Atoi(pageList[0])
+		if err != nil {
+			http.Error(w, "Invalid query parameter", http.StatusBadRequest)
+			return
+		}
+	}
+
+	resString := fmt.Sprintf("Article List (page %d)\n", page)
+	io.WriteString(w, resString)
 }
 
-// /article/1
+// /article/[id]
 func ArticleDetailHandler(w http.ResponseWriter, req *http.Request) {
-	articleID := 1
+	articleID, err := strconv.Atoi(mux.Vars(req)["id"])
+	if err != nil {
+		http.Error(w, "Invalid path parameter", http.StatusBadRequest)
+		return
+	}
 	resString := fmt.Sprintf("Article No.%d\n", articleID)
 	io.WriteString(w, resString)
 }
