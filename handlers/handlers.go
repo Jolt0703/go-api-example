3b112952a10a5ad04c8ba12651b49ebfe4c174f3
Matsuzaki Jotaro <midori1136@gmail.com>
2022-11-06 12:18:07 +0900

    記事投稿リクエストのjsonデータを解析する処理を追加

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"log"
@@ -19,9 +20,31 @@ func HelloHandler(w http.ResponseWriter, req *http.Request) {
 
 // /article
 func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
-	article1 := models.Article1
-	jsonData, err := json.Marshal(article1)
+	// リクエストヘッダのContent-Lengthからbodyの長さを取得
+	length, err := strconv.Atoi(req.Header.Get("Content-Length"))
+	if err != nil {
+		http.Error(w, "couldn't get content length\n", http.StatusBadRequest)
+		return
+	}
 
+	// リクエストボディのjsonデータを[]byteスライスに格納
+	reqBodybuffer := make([]byte, length)
+	if _, err := req.Body.Read(reqBodybuffer); !errors.Is(err, io.EOF) {
+		http.Error(w, "failed to get request body\n", http.StatusBadRequest)
+		return
+	}
+	defer req.Body.Close()
+
+	// jsonデータをGoの構造体に変換
+	var reqArticle models.Article
+	if err := json.Unmarshal(reqBodybuffer, &reqArticle); err != nil {
+		http.Error(w, "failed to decode json\n", http.StatusBadRequest)
+		return
+	}
+
+	// レスポンスのためにGoの構造体をjsonデータに再度変換
+	article := reqArticle
+	jsonData, err := json.Marshal(article)
 	if err != nil {
 		http.Error(w, "failed to encode json\n", http.StatusInternalServerError)
 		return
@@ -33,9 +56,11 @@ func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
 
 // /article/list?page=[x]
 func ArticleListHandler(w http.ResponseWriter, req *http.Request) {
+	// クエリパラメータのマップValuesを取得(type Values map[string][]string)
 	queryMap := req.URL.Query()
 	page := 1
 
+	// クエリパラメータから"page"キーの値を取得
 	if pageList, ok := queryMap["page"]; ok && len(pageList) > 0 {
 		var err error
 		page, err = strconv.Atoi(pageList[0])
@@ -45,12 +70,13 @@ func ArticleListHandler(w http.ResponseWriter, req *http.Request) {
 		}
 	}
 
+	// レスポンスのためにGoの構造体をjsonデータに変換
 	article1, article2 := models.Article1, models.Article2
 	articleList := []models.Article{article1, article2}
 	jsonData, err := json.Marshal(articleList)
 
 	if err != nil {
-		errMsg := fmt.Sprintf("fail to encode json (page %d)\n", page)
+		errMsg := fmt.Sprintf("failed to encode json (page %d)\n", page)
 		http.Error(w, errMsg, http.StatusInternalServerError)
 		return
 	}
@@ -61,6 +87,7 @@ func ArticleListHandler(w http.ResponseWriter, req *http.Request) {
 
 // /article/[id]
 func ArticleDetailHandler(w http.ResponseWriter, req *http.Request) {
+	// リクエストに含まれるパスパラメターのマップを取得しidを数値に変換
 	articleID, err := strconv.Atoi(mux.Vars(req)["id"])
 
 	if err != nil {
@@ -68,11 +95,12 @@ func ArticleDetailHandler(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
+	// レスポンスのためにGoの構造体をjsonデータに変換
 	article1 := models.Article1
 	jsonData, err := json.Marshal(article1)
 
 	if err != nil {
-		errMsg := fmt.Sprintf("fail to encode json (articleID %d)\n", articleID)
+		errMsg := fmt.Sprintf("failed to encode json (articleID %d)\n", articleID)
 		http.Error(w, errMsg, http.StatusInternalServerError)
 		return
 	}
@@ -83,6 +111,7 @@ func ArticleDetailHandler(w http.ResponseWriter, req *http.Request) {
 
 // /article/nice
 func PostNiceHandler(w http.ResponseWriter, req *http.Request) {
+	// レスポンスのためにGoの構造体をjsonデータに変換
 	article1 := models.Article1
 	jsonData, err := json.Marshal(article1)
 
@@ -96,6 +125,7 @@ func PostNiceHandler(w http.ResponseWriter, req *http.Request) {
 
 // /comment
 func PostCommentHandler(w http.ResponseWriter, req *http.Request) {
+	// レスポンスのためにGoの構造体をjsonデータに変換
 	comment1 := models.Comment1
 	jsonData, err := json.Marshal(comment1)
 
